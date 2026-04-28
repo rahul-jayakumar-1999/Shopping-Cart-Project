bb47eb5e3e4892d8f6373dd1ccc0185bc2571c34
rahuljayakumar <rahuljayakumarkuttan@gmail.com>
2026-04-29 00:51:42 +0300

    Create models and controllers folder, add sample image to public/image folder, add nodemodule and images to .gitignore, create layouts and partials, create basic navbar an product card layout from bootstrap, create basic product array and load cards, in app.js setup layout and partials

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const exphbs = require('express-handlebars');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +14,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+// setup handlebars
+app.engine('hbs', exphbs.engine({
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials'),
+  defaultLayout: 'layout'   // layout file name
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const products = [
+    {
+      name: "Wireless Mouse",
+      category: "Electronics",
+      description:
+        "Ergonomic wireless mouse with USB receiver and long battery life",
+      price: 15.99,
+      image: "../images/Wireless Mouse.webp",
+    },
+    {
+      name: "Running Shoes",
+      category: "Footwear",
+      description:
+        "Lightweight running shoes with breathable mesh and cushioned sole",
+      price: 49.99,
+      image: "../images/Running Shoes.jpg",
+    },
+    {
+      name: "Coffee Maker",
+      category: "Home Appliances",
+      description: "Automatic drip coffee maker with programmable timer",
+      price: 29.99,
+      image: "../images/Coffee Maker.jpg",
+    },
+    {
+      name: "Backpack",
+      category: "Accessories",
+      description:
+        "Durable backpack with multiple compartments and laptop sleeve",
+      price: 25.5,
+      image: "../images/Backpack.webp",
+    },
+    {
+      name: "Bluetooth Speaker",
+      category: "Electronics",
+      description:
+        "Portable speaker with high-quality sound and 10-hour battery",
+      price: 39.99,
+      image: "../images/Bluetooth Speaker.webp",
+    },
+  ];
+  res.render("index", { title: "Shopping Cart", products, admin: false });
 });
 
 module.exports = router;
