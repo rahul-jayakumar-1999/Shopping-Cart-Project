b7a378bba30da6a2a146b4b4fd4deebc7369b847
rahuljayakumar <rahuljayakumarkuttan@gmail.com>
2026-05-18 12:53:39 +0300

    add quantity

--- models/cartModel.js
+++ models/cartModel.js
@@ -5,20 +5,40 @@ const { ObjectId } = require("mongodb");
 module.exports = {
   addToCart: async function (userId, productId) {
     const db = getDB();
+    const productData = {
+      productID: new ObjectId(productId),
+      quantity: 1,
+    };
     const isCartExist = await db
       .collection(collection.CART_COLLECTION)
       .findOne({ userId: new ObjectId(userId) });
     if (isCartExist) {
-      return await db
-        .collection(collection.CART_COLLECTION)
-        .updateOne(
-          { userId: new ObjectId(userId) },
-          { $push: { products: new ObjectId(productId) } },
-        );
+      console.log(isCartExist.products);
+      let productExist = isCartExist.products.findIndex((product) => {
+        // console.log("1"+product.productID)
+        // console.log("2"+productId)
+        return product.productID.toString() === productId.toString();
+      });
+
+      if (productExist !== -1) {
+        return await db
+          .collection(collection.CART_COLLECTION)
+          .updateOne(
+            { "products.productID": new ObjectId(productId) },
+            { $inc: { "products.$.quantity": 1 } },
+          );
+      } else {
+        return await db
+          .collection(collection.CART_COLLECTION)
+          .updateOne(
+            { userId: new ObjectId(userId) },
+            { $push: { products: productData } },
+          );
+      }
     } else {
       const cart = {
         userId: new ObjectId(userId),
-        products: [new ObjectId(productId)],
+        products: [productData],
       };
       return await db.collection(collection.CART_COLLECTION).insertOne(cart);
     }
@@ -58,14 +78,12 @@ module.exports = {
     const isCartExist = await db
       .collection(collection.CART_COLLECTION)
       .findOne({ userId: new ObjectId(userId) });
-    
+
     if (isCartExist) {
       count = isCartExist.products.length;
       return count;
     } else {
       return count;
     }
-    
-    
-  }
+  },
 };
